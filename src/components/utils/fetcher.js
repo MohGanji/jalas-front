f803321e7b0bebbfc8d12ca937b578d5d3772748
Mohammad macbook <mfg1376@gmail.com>
2020-01-23 11:01:03 +0330

    change url to connect ot server

--- src/components/utils/fetcher.js
+++ src/components/utils/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const BASE_URL = 'http://127.0.0.1:8000'
-const BASE_URL = 'http://192.168.43.46:8000'
+const BASE_URL = 'http://team99.fall98.utse.ir/api'
 
 const config = async () => {
   return {
